c3a8becf7d5d045adb9f710ab84ec9d848d9a1b8
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-06-05 10:43:24 +0530

    updated register js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React , {useState} from "react";
 
 function Register({onRouteChange}) {
+  const [email , setEmail ] = useState(null);
+  const [password , setPassword] =  useState(null);
+  const [name , setName ] = useState('');
+
+  const handleNameChange  = (e) =>{
+    setName(e.target.value);
+  }
+  const handleEmailChange = (e) =>{
+
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = (e) =>{
+    setPassword(e.target.value);
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    onRouteChange('home')
+    
+  }
   return (
     <div className="bg-grey-lighter  min-h-screen flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-transparent px-6 py-8 rounded shadow-md text-black w-full">
+        <div className="bg-transparent px-6 py-8 rounded shadow-md text-white w-full">
           <h1 className="mb-8 text-3xl text-center">Sign up</h1>
           <input
             type="text"
-            className="block bg-black opacity-60 hover:opacity-100  border border-grey-light w-full p-3 rounded mb-4"
+            className="block bg-black opacity-60 hover:opacity-100   border border-grey-light w-full p-3 rounded mb-4"
             name="fullname"
             placeholder="Full Name"
+            onChange={handleNameChange}
           />
 
           <input
@@ -18,6 +38,7 @@ function Register({onRouteChange}) {
             className="block bg-black opacity-60 hover:opacity-100 border border-grey-light w-full p-3 rounded mb-4"
             name="email"
             placeholder="Email"
+            onChange={handleEmailChange}
           />
 
           <input
@@ -25,44 +46,26 @@ function Register({onRouteChange}) {
             className="block bg-black opacity-60 hover:opacity-100 border border-grey-light w-full p-3 rounded mb-4"
             name="password"
             placeholder="Password"
+            onChange={handlePasswordChange}
           />
-          <input
-            type="password"
-            className="block  bg-black opacity-60 hover:opacity-100 border border-grey-light w-full p-3 rounded mb-4"
-            name="confirm_password"
-            placeholder="Confirm Password"
-          />
+  
 
           <button
             type="submit"
             className="w-full bg-cyan-500 text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-            onClick={()=> onRouteChange('home')}
+            onClick={handleSubmit}
+
           >
             Create Account
           </button>
 
-          <div className="text-center text-sm text-grey-dark mt-4">
-            By signing up, you agree to the
-            <a
-              className="no-underline border-b border-grey-dark text-grey-dark"
-              href="#"
-            >
-              Terms of Service
-            </a>{" "}
-            and
-            <a
-              className="no-underline border-b border-grey-dark text-grey-dark"
-              href="#"
-            >
-              Privacy Policy
-            </a>
-          </div>
+   
         </div>
 
         <div className="text-grey-dark mt-6">
           Already have an account?
           <a
-            className="no-underline border-b border-blue text-blue "
+            className="no-underline border-b border-blue text-blue hover:cursor-pointer"
             onClick={()=> onRouteChange('signin')}
           >
             Log in
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn({ onRouteChange }) {
+  const [email , setEmail ] = useState(null);
+  const [password , setPassword] =  useState(null);
+
+  const handleEmailChange = (e) =>{
+
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = (e) =>{
+    setPassword(e.target.value);
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(email , password)
+    onRouteChange("home")
+  }
   return (
     <div className="bg-grey-lighter  min-h-screen flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
@@ -11,8 +26,8 @@ function SignIn({ onRouteChange }) {
                 Sign in to your account
               </h2>
             </div>
-            <form className="mt-8 space-y-6" action="#" method="POST">
-              <input type="hidden" name="remember" defaultValue="true" />
+            <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+              <input type="hidden" name="remember" defaultValue="true"  />
               <div className="space-y-6 rounded-md shadow-sm">
                 <div>
                   <label htmlFor="email-address" className="sr-only">
@@ -24,8 +39,9 @@ function SignIn({ onRouteChange }) {
                     type="email"
                     autoComplete="email"
                     required
-                    className="relative bg-black opacity-60 hover:opacity-100 block w-full rounded-t-md border-0 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:text-center"
+                    className="relative bg-black opacity-60 hover:opacity-100 block w-full rounded-t-md border-0 py-2 lg:text-lg text-white font-bold ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:text-center"
                     placeholder="Email address"
+                    onChange={handleEmailChange}
                   />
                 </div>
                 <div>
@@ -38,27 +54,29 @@ function SignIn({ onRouteChange }) {
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="relative bg-black opacity-60 hover:opacity-100 block w-full rounded-b-md border-0 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:text-center"
+                    className="relative bg-black opacity-60 hover:opacity-100 block w-full rounded-b-md border-0 py-2 text-white ring-1 text-xl ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:text-center"
                     placeholder="Password"
+                    onChange={handlePasswordChange}
                   />
                 </div>
               </div>
 
               <div className="flex items-center justify-between">
                 <div className="text-sm">
-                  <a
+                  <p
                     onClick={() => onRouteChange("register")}
                     className="font-medium text-indigo-600 hover:text-indigo-500"
                   >
                     Register
-                  </a>
+                  </p>
                 </div>
               </div>
 
               <div>
                 <button
-                  onClick={() => onRouteChange("home")}
+              
                   type="submit"
+              
                   className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
